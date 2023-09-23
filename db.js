7f14729b19ff19e3ade88279441564f23d49b9bd
Andres Sierra <asierra447@gmail.com>
2023-09-22 23:48:23 -0500

    cambios en db para ver si funciona

--- db.js
+++ db.js
@@ -2,13 +2,7 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-    POSTGRES_URL:"postgres://default:B5G7DLvOpKuf@ep-summer-firefly-75818562-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-    POSTGRES_PRISMA_URL:"postgres://default:B5G7DLvOpKuf@ep-summer-firefly-75818562-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
-    POSTGRES_URL_NON_POOLING:"postgres://default:B5G7DLvOpKuf@ep-summer-firefly-75818562.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-    POSTGRES_USER:"default",
-    POSTGRES_HOST:"ep-summer-firefly-75818562-pooler.us-east-1.postgres.vercel-storage.com",
-    POSTGRES_PASSWORD:"B5G7DLvOpKuf",
-    POSTGRES_DATABASE:"verceldb",
+    connectionString: "postgres://default:B5G7DLvOpKuf@ep-summer-firefly-75818562-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require"
 })
 
 /*const pool = new Pool({
